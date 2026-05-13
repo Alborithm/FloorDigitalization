358596912e896090ca0462085c6fa89bc0d48933
Gerardo Albor <47321228+Alborithm@users.noreply.github.com>
2026-05-13 16:46:26 -0600

    Models for multilinea created

---
+++ Multilinea/Models/Op10ATruck.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FloorDigitalization.Helpers;
+
+namespace FloorDigitalization.Multilinea.Models;
+
+public partial class Op10ATruck : ObservableValidator
+{
+    // ----------- DCP 22 -----------
+    [ObservableProperty] private bool _dcp22Start;
+    [ObservableProperty] private bool _dcp22Mid;
+    [ObservableProperty] private bool _dcp22End;
+
+    // ----------- DCP SN 33 -----------
+    private const double dcpSn33LowerLimit = -.40;
+    private const double dcpSn33UpperLimit = .40;
+
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn33LowerLimit, dcpSn33UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")] 
+    private decimal? _dcpSn33Start;
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn33LowerLimit, dcpSn33UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")]
+    private decimal? _dcpSn33Mid;
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn33LowerLimit, dcpSn33UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")]
+    private decimal? _dcpSn33End;
+
+    // ----------- DCP 23 -----------
+    [ObservableProperty] private bool _dcp23Start;
+    [ObservableProperty] private bool _dcp23Mid;
+    [ObservableProperty] private bool _dcp23End;
+
+    // ----------- DCP 34 -----------
+    [ObservableProperty] private bool _dcp34Start;
+    [ObservableProperty] private bool _dcp34Mid;
+    [ObservableProperty] private bool _dcp34End;
+}
---
+++ Multilinea/Models/Op10WTruck.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FloorDigitalization.Helpers;
+
+namespace FloorDigitalization.Multilinea.Models;
+
+public partial class Op10WTruck : ObservableValidator
+{
+    // ----------- DCP SN -----------
+    [ObservableProperty] private bool _dcpSnStart;
+    [ObservableProperty] private bool _dcpSnMid;
+
+    // ----------- DCP SN 20 -----------
+    private const double dcpSn20LowerLimit = -.25;
+    private const double dcpSn20UpperLimit = .25;
+
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn20LowerLimit, dcpSn20UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")] 
+    private decimal? _dcpSn20Start;
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn20LowerLimit, dcpSn20UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")]
+    private decimal? _dcpSn20Mid;
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn20LowerLimit, dcpSn20UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")]
+    private decimal? _dcpSn20End;
+
+    // ----------- DCP SN 81 -----------
+    private const double dcpSn81LowerLimit = -.25;
+    private const double dcpSn81UpperLimit = .25;
+
+    [ObservableProperty][NotifyDataErrorInfo]
+    [Range(dcpSn81LowerLimit, dcpSn81UpperLimit, ErrorMessage = "Valor fuera de especificación ({1} a {2})")]
+    private decimal? _dcpSn81Start;
+}
